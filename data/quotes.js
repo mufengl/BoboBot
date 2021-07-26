b3253c7c2ca9e23c62e1841058c137c15ee2cfbd
mufengl <59774645+mufengl@users.noreply.github.com>
2021-07-26 11:43:48 -0400

    Create quotes.js

---
+++ data/quotes.js
@@ -0,0 +1,38 @@
+let quotes = [
+	"To be, or not to be: that is the question.",
+	"All the world's a stage, and all the men and women merely players. They have their exits and their entrances; And one man in his time plays many parts.",
+	"What's in a name? A rose by any other name would smell as sweet.",
+	"A man takes his sadness and throws it away but then he's still left with his hands.",
+	"Tell me how all this, and love too, will ruin us.",
+	"You remember too much, my mother said to me recently. Why hold onto all that? And I said, where can I put it down?",
+	"And now here is my secret, a very simple secret: It is only with the heart that one can see rightly; what is essential is invisible to the eye.",
+	"People have forgotten this truth, the fox said. But you mustn't forget it. You become responsible forever for what you've tamed.",
+	"College was like a four-year game show called 'Do my friends hate me or do I just need to go to sleep?'",
+	"You remember being 12 when you're like 'No one will look at me or I'll kill myself'.",
+	"My vibe is like, hey you could probably pour soup in my lap and I'll apologize to you.",
+	"Captain Wuntch. Good to see you. But if you're here, who's guarding Hades?",
+	"We shall not cease from exploration and the end of all our exploring will be to arrive where we started and know the place for the first time.",
+	"Why were we made just so, to find so many things that happens everyday pretty.",
+	"They cannot scare me with their empty spaces between stars - on stars where no human race is.",
+	"What we call the beginning is often the end and to make an end is to make a beginning. The end is where we start.",
+	"Because one has only learnt to get the better of words for the thing one no longer has to say.",
+	"For us, there is only the trying. The rest is not our business.",
+	"Life's but a walking shadow, a poor player, that struts and frets his hour upon the stage and then is heard no more.",
+	"They say there's no future for us. They're right. Which is fine by us.",
+	"I may not have gone where I intended to go, but I think I have ended up where I needed to be.", 
+	"The story so far: In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.",
+	"I refuse to answer that question on the grounds that I don't know the answer.",
+	"The ships hung in the sky in much the same way that bricks don't.",
+	"For a moment, nothing happened. Then, after a second or so, nothing continued to happen.",
+	"If there's anything more important than my ego around, I want it caught and shot now.",
+	"I haven't the slightest idea how to change people, but still I keep a long list of prospective candidates just in case I should ever figure it out.",
+	"I sleep. I dream. I make up things that I would never say. I say them very quietly.",
+	"Keep your face always toward the sunshine - and shadows will fall behind you.",
+	"Happiness, not in another place but this place... not for another hour, but this hour.",
+	"I am as bad as the worst, but, thank God, I am as good as the best.",
+	"The art of art, the glory of expression and the sunshine of the light of letters, is simplicity."
+];
+
+module.exports = {
+	quoteList: quotes
+}
